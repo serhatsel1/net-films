127b40271e54cc3f92500c088aaef38c31fb400e
Serhat Sel <selserhat01@gmail.com>
2024-03-16 02:40:08 +0300

    category detail page

--- containers/home/index.jsx
+++ containers/home/index.jsx
@@ -6,11 +6,15 @@ import MoviesSection from "@/components/movies-section";
 import Movies from "@/mocks/movies.json";
 import Genres from "@/mocks/genres.json";
 
-const HomeContainer = () => {
+const HomeContainer = ({ selectedCategory }) => {
+  console.log(selectedCategory)
   return (
     <>
       <FeaturedMovie movie={Movies.results[0]} />
       <Categories categories={Genres.genres.slice(0, 5)} />
+      {selectedCategory?.movies.length > 0 && (
+        <MoviesSection title={Genres.genres.find((genre) => genre.id === parseInt(selectedCategory.id)).name} movies={selectedCategory?.movies} />
+      )}
       <MoviesSection
         title="Populer Films"
         movies={Movies.results.slice(1, 8)}
