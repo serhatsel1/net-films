7e4621e7c5d1c5367a888ce476f875c726dd0c58
Serhat Sel <selserhat01@gmail.com>
2024-03-16 01:54:32 +0300

    It's the same as the previous one, only the error is different.

--- app/movie/[id]/page.jsx
+++ app/movie/[id]/page.jsx
@@ -2,18 +2,18 @@ import MovieContainer from "@/containers/movie";
 import Movies from "@/mocks/movies.json";
 import { notFound } from "next/navigation";
 
-const MoviePage = ({ params }) => {
-
+const MoviePage = ({ params, searchParams }) => {
   const movieDetails = Movies.results.find((movie) => {
     return movie.id === parseInt(params.id);
   });
 
-
-  if(!movieDetails) {
-    notFound()
+  if (!movieDetails) {
+    notFound();
   }
 
-
+  if (searchParams.error === "true") {
+    throw new error("dangerous error !!!");
+  }
 
   return <MovieContainer movie={movieDetails} />;
 };
