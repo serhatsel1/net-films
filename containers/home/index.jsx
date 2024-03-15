7e6f7b79e7a7d166199c2df97d7da6d56d44a28d
Serhat Sel <selserhat01@gmail.com>
2024-03-16 01:28:04 +0300

    Home Page 2

--- containers/home/index.jsx
+++ containers/home/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import FeaturedMovie from "@/components/featured-movie";
 import Categories from "@/components/categories";
+import MoviesSection from "@/components/movies-section";
 
 import Movies from "@/mocks/movies.json";
 import Genres from "@/mocks/genres.json";
@@ -10,6 +11,14 @@ const HomeContainer = () => {
     <>
       <FeaturedMovie movie={Movies.results[0]} />
       <Categories categories={Genres.genres.slice(0, 5)} />
+      <MoviesSection
+        title="Populer Films"
+        movies={Movies.results.slice(1, 8)}
+      />
+      <MoviesSection
+        title="Your Favorites"
+        movies={Movies.results.slice(8, 14)}
+      />
     </>
   );
 };
