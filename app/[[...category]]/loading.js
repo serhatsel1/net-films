3001e8b565accca6cda4fc1162cfc098f53c3e3c
Serhat Sel <selserhat01@gmail.com>
2024-03-17 03:00:13 +0300

    added MoviesSectionLoading

--- app/[[...category]]/loading.js
+++ app/[[...category]]/loading.js
@@ -1,11 +1,15 @@
 import CategoriesLoading from "@/components/categories/loading.js";
 import FeatureMovieLoading from "@/components/featured-movie/loading";
+import MoviesSectionLoading from "@/components/movies-section/loading";
 
 const loading = () => {
   return (
     <>
       <FeatureMovieLoading />
       <CategoriesLoading />
+      <MoviesSectionLoading />
+      <MoviesSectionLoading />
+      <MoviesSectionLoading />
     </>
   );
 };
